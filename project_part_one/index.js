17d047eb255340721b00cc4afae9256f54a4ca33
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-04-17 20:13:10 -0600

    Project 1 challenge 1

---
+++ project_part_one/index.js
@@ -0,0 +1,15 @@
+const app = (
+    <div>
+      <img src="./react-logo.png" alt="reactLogo" width="50" height="60"/>
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li>Was first released in 2003</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+  
+  ReactDOM.render(app, document.getElementById('root'))
